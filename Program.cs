bc794c124fb8eede50ee64d5afd2cab9a68d2642
Alex <zarjrw1994@gmail.com>
2023-11-27 23:06:47 +0300

    Not changed

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ Person nika = new Person("Nika", "Tarasova", "female", "11.12.1994", "0000SEKRET
 Patient<string> max = new Patient<string>("Maxim", "Smirnov","male", "11.11.1996", "9999SEKRET33CODE", "BY", "Minsk", "Minsk", "Odoevskogo", 34,12);
 Doctor<string> vika = new Doctor<string>("Vika", "Glyshko", "female", "10.10.1998", "111SEKRET888CODE", "BY", "Grodno", "Grodno", "Masherova", 56, 73);
 
+Console.WriteLine("Разные версии метода отображающего идентификационный номер");
 Console.WriteLine(nika.GetIdentificationNumber()); // Вирутальный метод
 Console.WriteLine(max.GetIdentificationNumber()); // Переопредленный метод
 Console.WriteLine(vika.GetIdentificationNumber()); // Скрытие метода
